4ee39ab35c09b2374eb396c35a65e1c0ee8bb527
Irene Tsang <tsangchiyan@Irenes-MacBook-Pro.local>
2024-07-08 22:01:36 -0400

    Header Added -> including resposive styles, functionality

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,16 +1,32 @@
 import "./Header.scss";
 import {Link} from "react-router-dom";
 import logo from "../../assets/logo/logo.svg";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+
+    const [activePage, setActivePage] = useState('closet');
+    const location = useLocation();
+
+    useEffect(() => {
+        const extractedItem = (location.pathname.split("/")[1]);
+        extractedItem === "" ? setActivePage('closet') : setActivePage(extractedItem);
+    }, [location]);
+
   return (
     <header className="header">
-        <Link to="/"><img className="header__logo" src={logo} alt="Logo"/></Link>
-        <nav className="header__nav">
-            <span>Closet</span>
-            <span>Outfit</span>
+        <nav className="nav">
+        <Link to="/"><img className="nav__logo" src={logo} alt="Logo"/></Link>
+        <div className="nav__links">
+            <Link to="/closet" className={`nav__link ${activePage === 'closet' ? 'nav__link--active' : ''}`}>
+                <span className="nav__text">Closet</span>
+            </Link>
+            <Link to="/outfit" className={`nav__link ${activePage === 'outfit' ? 'nav__link--active' : ''}`}>
+                <span className="nav__text">Outfit</span>
+            </Link>
+        </div>
         </nav>
-      
     </header>
   )
 }
