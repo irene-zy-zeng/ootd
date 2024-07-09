0b9fa3b632202eec9fd78fee273bda1f6bcb9c25
Irene Tsang <tsangchiyan@Irenes-MacBook-Pro.local>
2024-07-08 21:01:31 -0400

    added basic react routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,36 @@
-import './App.scss'
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ClosetPage from "./pages/ClosetPage/ClosetPage";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import UploadPage from "./pages/UploadPage/UploadPage";
+import ItemDetailsPage from "./pages/ItemDetailsPage/ItemDetailsPage";
+import EditDetailsPage from "./pages/EditDetailsPage/EditDetailsPage";
+import OutfitPage from "./pages/OutfitPage/OutfitPage";
+import AddOutfitPage from "./pages/AddOutfitPage/AddOutfitPage";
+
 
 function App() {
-
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<ClosetPage />} />
 
+          <Route path="/closet" element={<ClosetPage />} />
+          <Route path="/closet/:id" element={<ItemDetailsPage />} />
+          <Route path="/closet/:id/edit" element={<EditDetailsPage />} />
+          <Route path="/upload" element={<UploadPage />} />
+
+          <Route path="/outfit" element={<OutfitPage />} />
+          <Route path="/outfit/new" element={<AddOutfitPage />} />
+
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
