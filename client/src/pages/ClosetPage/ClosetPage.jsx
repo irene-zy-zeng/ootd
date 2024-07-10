261cd950406d00bee6665cfc637a4025f4017e04
Irene Tsang <tsangchiyan@Irenes-MacBook-Pro.local>
2024-07-10 16:46:46 -0400

    Finished axios call to GET all items, added responsive design for closet page

--- client/src/pages/ClosetPage/ClosetPage.jsx
+++ client/src/pages/ClosetPage/ClosetPage.jsx
@@ -1,14 +1,60 @@
-import Button from "../../components/Button/Button"
+import Button from "../../components/Button/Button";
 import AddIcon from "../../assets/icons/add.svg";
 import SortIcon from "../../assets/icons/sort.svg";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./ClosetPage.scss";
+
+const apiURL = import.meta.env.VITE_API_URL;
 
 const ClosetPage = () => {
-  return (
-    <div className="button-container">
-      <Button buttonVariant="primary" buttonLabel="Add New Item" buttonIcon={AddIcon} />
-      <Button buttonVariant="secondary" buttonLabel="Sort By ..." buttonIcon={SortIcon} />
-    </div>
-  )
-}
+  const [itemsData, setItemsData] = useState([]);
 
-export default ClosetPage
+  const getAllItems = async () => {
+    try {
+      const res = await axios.get(`${apiURL}/closet`);
+      const itemsData = res.data;
+      // console.log(itemsData);
+      setItemsData(itemsData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllItems();
+  }, []);
+
+  //Function to group items -> give you an array of catogories including an array of items
+  const groupedItems = Object.groupBy(itemsData,({category})=> category);
+  // console.log(groupedItems);
+
+  return (
+    <>
+      <div className="button-container">
+        <Button buttonVariant="primary" buttonLabel="Add New Item" buttonIcon={AddIcon}/>
+        <Button buttonVariant="secondary" buttonLabel="Sort By ..." buttonIcon={SortIcon} />
+      </div>
+    <section className="allItems">
+      {itemsData == null ? (
+        <p>Loading...</p>
+        ) : (
+          Object.keys(groupedItems).map((category)=>(
+            <div key={category} className="catogory-group">
+              <h2 className="catogory-group__title sub-header">{category}</h2>
+              <div className="catogory-group__content">
+                {groupedItems[category].map((item) => (
+                  <div key={item.id} className="catogory-group__image-card">
+                    <img src={item.image} alt={item.name} className="catogory-group__image"/>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))
+      )}
+    </section>
+    </>
+  );
+  };
+
+  export default ClosetPage;
