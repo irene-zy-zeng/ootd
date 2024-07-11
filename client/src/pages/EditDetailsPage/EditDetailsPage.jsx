6219b1ef205694901166da7b86e46d730f8dcdce
Irene Tsang <tsangchiyan@Irenes-MacBook-Pro.local>
2024-07-11 11:30:45 -0400

    Finished Responsive design and styling for ItemdDetails Page

--- client/src/pages/EditDetailsPage/EditDetailsPage.jsx
+++ client/src/pages/EditDetailsPage/EditDetailsPage.jsx
@@ -2,7 +2,13 @@
 const EditDetailsPage = () => {
   return (
     <div>
-      
+        <form className="form">
+        <label className="form__title sub-header">{selectedItem.name}</label>
+        <TextInput name="catogory" label="CATOGORY" placeholder="CATOGORY OF THE ITEM" value={selectedItem.category}/>
+        <TextInput name="color" label="COLOR" placeholder="COLOR OF THE ITEM" value={selectedItem.color}/>
+        <TextInput name="season" label="SEASON" placeholder="SEASON OF THE ITEM" value={selectedItem.season}/>
+        <TextInput name="brand" label="BRAND" placeholder="BRAND OF THE ITEM" value={selectedItem.brand}/>
+      </form>
     </div>
   )
 }
--- client/src/pages/ItemDetailsPage/ItemDetailsPage.jsx
+++ client/src/pages/ItemDetailsPage/ItemDetailsPage.jsx
@@ -5,6 +5,7 @@ import Button from "../../components/Button/Button";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import "./ItemDetailsPage.scss";
 
 const apiURL = import.meta.env.VITE_API_URL;
 
@@ -12,44 +13,65 @@ const ItemDetailsPage = (selectedItemId) => {
   const [selectedItem, setSeletedItem] = useState(null);
   const { id } = useParams();
 
-
-  const getSelectedItem = async (itemId)=>{
+  const getSelectedItem = async (itemId) => {
     try {
       const res = await axios.get(`${apiURL}/item/${id}`);
-      setSeletedItem(res.data)
-      console.log(res.data)
+      setSeletedItem(res.data);
+      console.log(res.data);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  useEffect(()=>{
-    getSelectedItem(selectedItemId)
-  },[selectedItemId])
+  useEffect(() => {
+    getSelectedItem(selectedItemId);
+  }, [selectedItemId]);
 
   if (selectedItem === null) {
     return <div>loading...</div>;
-}
+  }
 
   return (
     <div>
-      <Subheader titleText="Details"/>
+      <Subheader titleText="Details" />
+      <main className="item-details">
+      <div className="item-details__title">
+        <h2 className="sub-header">{selectedItem.name}</h2>
+      </div>
+    <div className="item-details__content">
       <div className="item-details__image-card">
         <img src={selectedItem.image} alt={selectedItem.name} className="item-details__image"/>
       </div>
-      <form className="form">
-        <label className="form__title sub-header">{selectedItem.name}</label>
-        <TextInput name="catogory" label="CATOGORY" placeholder="CATOGORY OF THE ITEM" value={selectedItem.category}/>
-        <TextInput name="color" label="COLOR" placeholder="COLOR OF THE ITEM" value={selectedItem.color}/>
-        <TextInput name="season" label="SEASON" placeholder="SEASON OF THE ITEM" value={selectedItem.season}/>
-        <TextInput name="brand" label="BRAND" placeholder="BRAND OF THE ITEM" value={selectedItem.brand}/>
-      </form>
-      <div className="item-details__button">
-      <Button buttonVariant="delete" buttonLabel="Delete"/>
-      <Button buttonVariant="primary" buttonLabel="Edit"/>
+      <div className="item-details__detials">
+        <div className="item-details__detials-group">
+          <label className="item-details__label label-text">CATOGORY</label>
+          <p className="item-details__text body-medium">
+            {selectedItem.category}
+          </p>
+        </div>
+        <div className="item-details__detials-group">
+          <label className="item-details__label label-text">COLOR</label>
+          <p className="item-details__text body-medium">{selectedItem.color}</p>
+        </div>
+        <div className="item-details__detials-group">
+          <label className="item-details__label label-text">SEASON</label>
+          <p className="item-details__text body-medium">
+            {selectedItem.season}
+          </p>
+        </div>
+        <div className="item-details__detials-group">
+          <label className="item-details__label label-text">BRAND</label>
+          <p className="item-details__text body-medium">{selectedItem.brand}</p>
+        </div>
+        <div className="item-details__button">
+          <Button buttonVariant="delete" buttonLabel="Delete" />
+          <Button buttonVariant="primary" buttonLabel="Edit" />
+        </div>
       </div>
+      </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default ItemDetailsPage
+export default ItemDetailsPage;
