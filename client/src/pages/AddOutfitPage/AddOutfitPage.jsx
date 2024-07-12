2cf72d2994eb0790949447c8a36178990d227efd
Irene Tsang <tsangchiyan@Irenes-MacBook-Pro.local>
2024-07-12 10:45:00 -0400

    Added buttons for NewOutfit page

--- client/src/pages/AddOutfitPage/AddOutfitPage.jsx
+++ client/src/pages/AddOutfitPage/AddOutfitPage.jsx
@@ -1,8 +1,15 @@
+import "./AddOutfitPage.scss";
+import Button from "../../components/Button/Button";
+import AddIcon from "../../assets/icons/add.svg";
+import ApparelIcon from "../../assets/icons/apparel.svg"
 
 const AddOutfitPage = () => {
   return (
     <div>
-      
+        <div className="button-container">
+         <Button buttonVariant="secondary" buttonLabel="All Outfit" buttonIcon={ApparelIcon} onClickAction={() => navigate("/outfit")}/>
+          <Button buttonVariant="primary" buttonLabel="Add New Outfit" buttonIcon={AddIcon} />
+      </div>
     </div>
   )
 }
