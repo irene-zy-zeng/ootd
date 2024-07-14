917c56254afd3e04939deba546a79f6b46077780
Irene Tsang <tsangchiyan@Irenes-MacBook-Pro.local>
2024-07-14 11:48:50 -0400

    Added Styling to 404page

--- client/src/pages/PageNotFound/PageNotFound.jsx
+++ client/src/pages/PageNotFound/PageNotFound.jsx
@@ -1,8 +1,9 @@
+import "./PageNotFound.scss";
 
 const PageNotFound = () => {
   return (
-    <div>
-      <p>Error: 404 Not Found</p>
+    <div className="error-page">
+      <h2 className="sub-header">Error: 404 Not Found...</h2>
     </div>
   )
 }
